4e31474e79da33ee554691d291e5b3d2ca9ffccd
Alexander Dmitriev <alex4056546@yandex.ru>
2023-02-09 02:28:53 +1100

    start frontend part

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page d-flex flex-column flex-column-fluid">
+    <PageHeader/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import PageHeader from '@/components/PageHeader.vue'
+export default{
+  components:{
+    PageHeader
+  }
+}
+</script>
---
+++ client/src/components/PageHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header align-items-stretch">
+        <div class="d-flex align-items-stretch justify-content-beetween container-xxl">
+            <div class="d-flex align-items-center ">
+                <a href="#">
+                    <img src="@/assets/logo.svg" alt="">
+                </a>
+            </div>
+            <div class="d-flex align-items-center ">
+                <input type="text" name="" id="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.header{
+    background-color: var(--bg-color-header-light);
+    display: flex;
+    top: 0;
+    right:0;
+    left:0;
+    position: fixed;
+    z-index: 102;
+    height: 94px;
+    padding: 0;
+    border-bottom: 1px solid #E4E6EF;
+}
+</style>
